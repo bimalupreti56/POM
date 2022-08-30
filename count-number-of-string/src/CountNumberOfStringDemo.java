d4b8db9119f67b4a15ee2ad45f46fdd370de6f58
upretibimal17 <upretibimal17@gmail.com>
2022-08-29 21:49:52 -0400

    A

--- count-number-of-string/src/CountNumberOfStringDemo.java
+++ count-number-of-string/src/CountNumberOfStringDemo.java
@@ -3,13 +3,19 @@ public class CountNumberOfStringDemo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String input = "Welcome to the Java Programming Basic Course";
-		
+		String input = "Welcome";
+		String rev = "";
 		int count = 0;
 		for (int i = 0; i < input.length(); i++) {
 			if (input.charAt(i) != ' ')
 				count += 1;
 		}
+		
+		// reversing a string
+		for (int i = input.length()-1;i >= 0; i--) {
+			rev += input.charAt(i);
+		}
+		System.out.println(rev);
 		System.out.println("Count is: "+count);
 	}
 
